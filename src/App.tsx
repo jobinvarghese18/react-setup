c2691ef6b607a204b2490632227dfe6676ccd408
Jobin T v <jobinvarghese18@gmail.com>
2022-10-01 20:40:29 +0700

    feat: type script

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import React from 'react';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" exact element={<Home />} />
+        <Route path="/"  element={<Home />} />
       </Routes>
+      
     </BrowserRouter>
   );
 };
